e8b21edfc48412907d86a231e49b2014fd13683b
onsunsl <onsunsl@foxmail.com>
2022-09-15 18:51:43 +0800

    kivy在TextInput输入焦点有效时候，点击其他控件时候输入框不丢焦点

---
+++ note/demo/kivy_demo/text_input_demo.py
@@ -0,0 +1,39 @@
+"""在TextInput输入焦点有效时候，点击其他控件时候输入框不丢焦点"""
+
+from kivy import app
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+
+
+class IgnoredFocusBehavior(Widget):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if touch not in FocusBehavior.ignored_touch:
+                FocusBehavior.ignored_touch.append(touch)
+        return super().on_touch_down(touch)
+
+
+class MyButton(IgnoredFocusBehavior, Button):
+    pass
+
+
+class MyLabel(IgnoredFocusBehavior, Label):
+    pass
+
+
+class MyApp(app.App):
+    def build(self):
+        box = BoxLayout()
+        box.add_widget(TextInput(focus=True))
+        box.add_widget(MyLabel(text="Label"))
+        box.add_widget(MyButton(text="Button"))
+        return box
+
+
+a = MyApp()
+a.run()
+
