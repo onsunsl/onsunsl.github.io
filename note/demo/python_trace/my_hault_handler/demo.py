5633363296cd7cb25172ac700d7ba774749f0abd
onsunsl <onsunsl@foxmail.com>
2022-10-19 16:59:48 +0800

    fault handler 说明

--- note/demo/python_trace/my_hault_handler/demo.py
+++ note/demo/python_trace/my_hault_handler/demo.py
@@ -4,6 +4,12 @@ from multiprocessing import Process
 
 import faulthandler2
 
+"""
+保持异常时候的调用栈： 类似内存非法访问的异常try except 也能捕获，但是fault handler 是兜底方案
+C 层面实现记录：异常时间、进程和线程栈， 支持多进程但是file参数需要以`a`追加方式打开
+
+"""
+
 
 def hello(*args, **kwargs):
     code = args[0]
@@ -20,14 +26,16 @@ print(faulthandler2.is_enabled())
 
 
 class CustomerProcess(Process):
+    """子进程异常示例"""
 
     def run(self):
         print(f"sub pid:{os.getpid()}, ppid:{os.getppid()}")
-        import ctypes
-        ctypes.string_at(0)
-
-        while True:
-            time.sleep(1)
+        try:
+            # 这里不捕获异常程序将退出
+            import ctypes
+            ctypes.string_at(0)
+        except Exception as err:
+            print(f"CustomerProcess error: {err}")
 
 
 if __name__ == '__main__':
@@ -38,6 +46,7 @@ if __name__ == '__main__':
         time.sleep(1)
 
     try:
+        # 这里不捕获异常程序将退出
         import ctypes
         ctypes.string_at(0)
     except BaseException as e:
